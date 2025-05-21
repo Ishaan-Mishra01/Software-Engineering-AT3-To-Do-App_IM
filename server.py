056b5a25ee7778e3d30b152f9e79a678019f64f2
Ishaan-Mishra01 <158523119+Ishaan-Mishra01@users.noreply.github.com>
2025-05-21 13:57:51 +1000

    Created a sort of backend and have a proper login page with some functionality (mostly looks nice rn)

--- server.py
+++ server.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, flash
 
 app = Flask(__name__)
 
-@app.route('/', methods= ['GET, POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	return render_template("home.html")
+	return redirect(url_for('login'))
 
-@app.route('/login', methods= ['GET, POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-	return render_template("login.html")
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        
+        error_msg = ""
+        if len(email) <= 4:
+                error_msg = "Email must be between 4 and 20 characters"
+        elif len(username) <= 5:
+              
+                error_msg = "Username must be between 5 and _ characters"
+        elif len(password) <= 8:
+                error_msg = "Username must be between 5 and _ characters"
+              #return must have and email\
+        if error_msg =="":
+               return render_template("home.html")
+        else:
+               flash(error_msg)
+	    
+
 
 if __name__ == '__main__':
 	app.run()
