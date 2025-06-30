dda5702ce923c483be16aeaab84e2ea91b1b4e9f
Ishaan-Mishra01 <158523119+Ishaan-Mishra01@users.noreply.github.com>
2025-06-30 12:58:53 +1000

    Added more comments on this file over the last 2-3 days

--- static/js/breakout.js
+++ static/js/breakout.js
@@ -1,7 +1,7 @@
-//score
+//score initalisation
 let score= 0;
 
-// Ball
+// Ball object and functionality
 
 class Ball {
 	constructor(x, y, radius, speedX, speedY) { // inital characteristics: position - (x,y) coords, radius is size and speed is speed in (x,y) direction
@@ -11,23 +11,23 @@ class Ball {
 		this.speedX = speedX;
 		this.speedY = speedY;
 	}
-	draw(context) { //this allows the ball to exist. Context is a param that refers to "CanvasRenderingContext2D" which is being passed in
-		context.beginPath();
-		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-		context.fillStyle = "purple";
-		context.fill();
-		context.closePath();
+	draw(context) { //this allows the ball to exist. Context is a param that refers to "CanvasRenderingContext2D" which is being passed in which allows the drawing capabilites of canvas to be used
+		context.beginPath(); //Begin path is how we start drawin on canvas
+		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);//drawing circle thru the arc method. Last 2 things are start and end angle(in radians).
+		context.fillStyle = "red"; // This is the colour
+		context.fill(); //this applies the fill style
+		context.closePath(); // close the drawing path. Basically 'BEGIN' and 'END' keywords in pseudocode.
 	}
-	update() {
-		this.x += this.speedX;
-		this.y += this.speedY;
+	update() { //method to make ball move in canvas. Called within game loop to make the pos of the ball change over time
+		this.x += this.speedX; // updates the current pos of the ball in horizontal
+		this.y += this.speedY; // same as above but for vertical
 	}
 }
 
-//Paddle
+//Paddle object and functionality
 
 class Paddle {
-	constructor(x, y, width, height, speed){
+	constructor(x, y, width, height, speed){ //size is width and height, and speed is only rectilinear, unlike ball.
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -36,7 +36,7 @@ class Paddle {
 	}
 	draw(context) {
 		context.fillStyle = 'orange';
-		context.fillRect(this.x, this.y, this.width, this.height);
+		context.fillRect(this.x, this.y, this.width, this.height); // .fillRect is to create and colour in rectangles
 
 	}
 	move(direction) {
@@ -57,20 +57,19 @@ class Brick {
 	}
 	draw(context){
 		if(this.status === 1){
-			context.fillStyle = 'yellow';
+			context.fillStyle = 'green';
 			context.fillRect(this.x, this.y, this.width, this.height);
 		}
 	}
 }
 
+const canvas = document.getElementById("canvas"); 
+const context = canvas.getContext("2d"); //use get context method to access the "context" of the objects in order to utilise and draw them on the canvas
+// param of .getContext() is the context type, which here is 2D as the game is in 2 dimentions.
+const ball = new Ball(200, 200, 7, 2, 2); //new instance of the ball to test/see if it works, thru the "new" keyword. 
+const paddle = new Paddle(175, canvas.height-10,100,10, 15); //the canvas.height-10 is to place the canvas at the bottom
+//const creates an immutable variable
 
-
-
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-
-const ball = new Ball(200, 200, 7, -2, -2);
-const paddle = new Paddle(175, canvas.height-10,100,10, 15);
 
 //brick wall
 
@@ -116,9 +115,10 @@ function drawBricks(){
 //paddle control
 
 document.addEventListener("keydown", (event)=>{
-	if(event.key === "ArrowLeft") {
+	if(event.key === "ArrowLeft") { //"keydown" event is when key is pressed.
+//Additionally, '===' is the JS equivalent of python '==', because in JS, 2 equality signs do not represent a strict equality, rather "they attempt to coerce the value, if they are of different types," To quote Douglas Crockford's excellent JavaScript: The Good Parts.
 		paddle.move(-1);
-	} else if(event.key === "ArrowRight"){
+	} else if(event.key === "ArrowRight"){ // 'else if' rather than elif ;-; 
 		paddle.move(1);
 	}
 	//cheat
@@ -161,19 +161,19 @@ function gameLoop(){
 	
 	//ball collision detection
 	 //sides
-	if(ball.x - ball.radius <0 || ball.x + ball.radius > canvas.width){
+	if(ball.x - ball.radius <0 || ball.x + ball.radius > canvas.width){ // '||' is or (the 2 straight lines)
 		ball.speedX = -ball.speedX;
 	}
 
 	//top side
 
-	if(ball.y - ball.radius < 0) {
+	if(ball.y - ball.radius < 0) { // similar if statement to the sides, but instead of horizontal, it applies for verticle
 		ball.speedY = -ball.speedY;
 	}
 	
 	//paddle collision detection
 
-	if( ball.x + ball.radius > paddle.x &&
+	if( ball.x + ball.radius > paddle.x && // && is symbol for and
 		ball.x - ball.radius < paddle.x + paddle.width &&
 		ball.y + ball.radius > paddle.y){
 			ball.speedY = -ball.speedY;
@@ -196,7 +196,7 @@ function gameLoop(){
 	drawBricks()
 
 	//loop
-	requestAnimationFrame(gameLoop);
+	requestAnimationFrame(gameLoop); //built in JS function for smooth animation
 
 }
 
